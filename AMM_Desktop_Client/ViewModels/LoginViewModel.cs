dca3d96a30018c4fb2a5b9a468f0199c99bfb7ca
Алексей Ефременко <alexeyefr@gmail.com>
2018-10-23 23:11:35 +0300

    2018-10-23_23:11

---
+++ AMM_Desktop_Client/ViewModels/LoginViewModel.cs
@@ -0,0 +1,39 @@
+﻿using Catel.MVVM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMM_Desktop_Client.ViewModels
+{
+    class LoginViewModel: ViewModelBase
+    {
+        public LoginViewModel()
+        {
+
+        }
+        
+        public override string Title { get { return "AMM_Desktop_Client"; } }
+
+        // TODO: Register models with the vmpropmodel codesnippet
+        // TODO: Register view model properties with the vmprop or vmpropviewmodeltomodel codesnippets
+        // TODO: Register commands with the vmcommand or vmcommandwithcanexecute codesnippets
+
+        protected override async Task InitializeAsync()
+        {
+            await base.InitializeAsync();
+
+            // TODO: subscribe to events here
+        }
+
+        protected override async Task CloseAsync()
+        {
+            // TODO: unsubscribe from events here
+
+            await base.CloseAsync();
+        }
+    }
+
+
+}
