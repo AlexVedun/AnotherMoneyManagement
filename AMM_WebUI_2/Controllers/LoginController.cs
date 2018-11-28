61f40453ef5b8ae8dca0a93acf0e43f5c886eec8
Алексей Ефременко <alexeyefr@gmail.com>
2018-11-28 21:19:11 +0200

    AMM_WebUI_2: Login is in process

---
+++ AMM_WebUI_2/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using AMM_Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AMM_WebUI_2.Controllers
+{
+    public class LoginController : ApiController
+    {
+        private IRepository mRepository;
+
+        public LoginController(IRepository _repository)
+        {
+            mRepository = _repository;
+        }
+    }
+}
---
+++ AMM_WebUI_2/Models/LoginRequestForm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AMM_WebUI_2.Models
+{
+    public class LoginRequestForm
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
