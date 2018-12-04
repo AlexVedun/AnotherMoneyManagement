234c90fe50475f8fb44dedc1a82ef16b88e50ee2
Алексей Ефременко <alexeyefr@gmail.com>
2018-12-04 16:14:06 +0200

    04.12.2018_16:15

---
+++ AMM_WebUI_2/Controllers/ChunckController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web.Http;
+
+namespace AMM_WebUI_2.Controllers
+{
+    public class ChunckController : ApiController
+    {
+        [Route("api/chunck")]
+        public Object Get([FromUri] string _chunck)
+        {
+            return GetHTMLPageText(AppDomain.CurrentDomain.BaseDirectory + "\\wwwroot\\pages\\" + _chunck + ".htm");
+        }
+
+        private Object GetHTMLPageText(string _pageUri/*, string _param*/)
+        {
+            var response = new HttpResponseMessage();
+            string page = "";
+            using (StreamReader reader = new StreamReader(_pageUri))
+            {
+                page = reader.ReadToEnd();
+                //page = page.Replace("param", _param);
+            }
+            response.Content = new StringContent(page);
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("text/html");
+            return response;
+        }
+    }
+}
