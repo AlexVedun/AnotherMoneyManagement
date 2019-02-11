1885bc9e90aa11aef8ad158d743d4ad116a87f61
Efremenko Alexey <alexeyefr@gmail.com>
2019-02-11 20:33:47 +0200

    AMM_WebUI_2: мелкие улучшения

--- AMM_WebUI_2/Controllers/TransactionController.cs
+++ AMM_WebUI_2/Controllers/TransactionController.cs
@@ -76,68 +76,24 @@ namespace AMM_WebUI_2.Controllers
             transaction.From.Money -= transaction.Credit;
             transaction.To.Money += transaction.Debet;
 
-            try
+            if (transaction.From.Money < 0)
             {
-                mRepository.SourceAMM.SaveSource(transaction.From);
-                mRepository.SourceAMM.SaveSource(transaction.To);
-                mRepository.TransactionAMM.SaveTransaction(transaction);
-                return new ApiResponse<Transaction>() { data = transaction, error = "" };
+                return new ApiResponse<Transaction>() { error = "Недостаточно средств для выполнения транзакции!" };
             }
-            catch (Exception ex)
+            else
             {
-                return new ApiResponse<Transaction>() { error = ex.Message };
-            }
-
-            //Transaction transaction1 = new Transaction();
-            //Transaction transaction2 = new Transaction();
-            //TransactionLog transactionLog = new TransactionLog()
-            //{
-            //    Date = _transactionForm.Date,
-            //    Comment = _transactionForm.Comment,
-            //    User = mRepository.UserAMM.GetUserByLogin(login),
-            //    From = mRepository.SourceAMM.GetSourceById(login, _transactionForm.From),
-            //    To = mRepository.SourceAMM.GetSourceById(login, _transactionForm.To)
-            //};
-
-            //transaction1.Source = transactionLog.From;
-            //transaction1.TransactionLog = transactionLog;
-            //if (transactionLog.From.Type == TypeOfSource.Wallet || transactionLog.From.Type == TypeOfSource.Card)
-            //{
-            //    transaction1.Credit = _transactionForm.Summ;
-            //}
-            //else
-            //{
-            //    transaction1.Credit = 0;
-            //}
-
-            //transaction2.Source = transactionLog.To;
-            //transaction2.TransactionLog = transactionLog;
-            //if (transactionLog.To.Type == TypeOfSource.Wallet || transactionLog.To.Type == TypeOfSource.Card)
-            //{
-            //    transaction2.Debet = _transactionForm.Summ;
-            //}
-            //else
-            //{
-            //    transaction2.Debet = 0;
-            //}
-
-            //try
-            //{
-            //    transactionLog.From.Money -= transaction1.Credit;
-            //    transactionLog.To.Money += transaction2.Debet;
-            //    transactionLog.Status = TransStatus.Success;
-            //    mRepository.TransactionLogAMM.SaveTransactionLog(transactionLog);
-            //    mRepository.TransactionAMM.SaveTransaction(transaction1);
-            //    mRepository.TransactionAMM.SaveTransaction(transaction2);
-            //    //transactionLog.Transactions.Add(transaction1);
-            //    //transactionLog.Transactions.Add(transaction2);
-
-            //    return new ApiResponse<TransactionLog>() { data = transactionLog, error = "" };                
-            //}
-            //catch (Exception ex)
-            //{
-            //    return new ApiResponse<TransactionLog>() { error = ex.Message };
-            //}
+                try
+                {
+                    mRepository.SourceAMM.SaveSource(transaction.From);
+                    mRepository.SourceAMM.SaveSource(transaction.To);
+                    mRepository.TransactionAMM.SaveTransaction(transaction);
+                    return new ApiResponse<Transaction>() { data = transaction, error = "" };
+                }
+                catch (Exception ex)
+                {
+                    return new ApiResponse<Transaction>() { error = ex.Message };
+                }
+            }                       
         }
     }
 }
