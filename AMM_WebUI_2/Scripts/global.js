ccd591da56d2574ea8ee41a6fbbfb4cdd62a7522
Алексей Ефременко <alexeyefr@gmail.com>
2019-04-11 15:24:04 +0300

    Комментирование и чистка кода

--- AMM_WebUI_2/Scripts/global.js
+++ AMM_WebUI_2/Scripts/global.js
@@ -1,12 +1,15 @@
-﻿let isLogin = false;
-let userLogin = "";
-let isFamily = false;
+﻿// Глобальные переменные
+let isLogin = false;        // вошел ли пользователь в систему
+let userLogin = "";         // логин пользователя
+let isFamily = false;       // пока не используется
 
-const INCOME = 0;
-const WASTE = 1;
-const WALLET = 2;
-const CARD = 3;
+// Константы 
+const INCOME = 0;           // поступление денег
+const WASTE = 1;            // расход денег
+const WALLET = 2;           // кошелек
+const CARD = 3;             // карта
 
+// Функция для получения выбранного элемента выпадающего списка
 function GetSelectedItemId(_element) {
     let element = "#" + _element;
     return $(element + " option:selected").eq($(element).parent('li.selected').index()).val();
